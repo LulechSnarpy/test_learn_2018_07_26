743ca3e4dae9a59a30843e710adce2c1ee8499e0
LulechSnarpy <1272045703@qq.com>
2018-07-27 15:52:44 +0800

    2018_07_27_15_52

---
+++ src/datetype/DataTypeTest.java
@@ -0,0 +1,24 @@
+package datetype;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class DataTypeTest {
+	private static final ThreadLocal<DateFormat> df = new ThreadLocal<DateFormat>() {
+		@Override
+		protected DateFormat initialValue() {
+			return new SimpleDateFormat("yyyyMMddHHmmss");
+		}
+	};
+	
+	public static void main(String[] args) {
+		// "20091010121212"
+		DateFormat format = df.get();
+		String s = format.format(System.currentTimeMillis());
+		System.out.println(s);
+		
+		// JDK8 使用Instant代替Date,LocalDateTime代替Calendar,
+		// DateTimeFormatter代替SimpleDateFormat，官方給出的解釋:
+		// simple beautiful strong immutable thread-safe
+	}
+}
