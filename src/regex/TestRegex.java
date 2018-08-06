1692e8c83050bf8e95e19ccd62a7194095bed5b1
LulechSnarpy <1272045703@qq.com>
2018-08-06 09:02:44 +0800

    test is update?

--- src/regex/TestRegex.java
+++ src/regex/TestRegex.java
@@ -2,6 +2,21 @@ package regex;
 
 public class TestRegex {
 	public static void main(String[] args) {
+		//testStringRepalce1();
+		//""
+		//testStringRepacle2();
+		String reg = "(?<=\\.)(.*)";
+		String aim = "test2.chj.zj.sx";
+		System.out.println(aim.replaceAll(reg, "chj.zj.sh"));
+	}
+
+	private static void testStringRepacle2() {
+		String reg = "(?<=[\\d]{3})(.)(?=[\\d]{7})";
+		String aim = "13335351414";
+		System.out.println(aim.replaceAll(reg, "****"));
+	}
+
+	private static void testStringRepalce1() {
 		String reg = "(?<=\\.)(.*)";
 		String aim = "test2.chj.zj.sx";
 		System.out.println(aim.replaceAll(reg, "chj.zj.sh"));
